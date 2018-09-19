150ae9582be5a13a8941dc55beadbbe92d3dcce2
Wendell Clive Santos de Lira <wendell.clive@gmail.com>
2018-09-19 15:14:49 -0300

    Inserção de Lombok no codigo para menor Verbosidade

--- src/main/java/com/databuilder/com/br/escalafacil/domain/Escala.java
+++ src/main/java/com/databuilder/com/br/escalafacil/domain/Escala.java
@@ -11,6 +11,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,13 +21,15 @@ import lombok.Setter;
  * Created by Wendell Clive - email: wendell.clive@gmail.com 14/09/2018
  */
 
-@NoArgsConstructor
+
 @Entity
 @Table(name = "escala")
-@Getter
-@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Setter
 public class Escala implements Serializable {
 
+	
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -41,42 +44,5 @@ public class Escala implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "proprietario_id")
 	private Proprietario proprietario;
-
-	public Escala(Integer id, String instituicao, String finalidade, String titulo, String tipo, Date dataCriacao,
-			Proprietario proprietario) {
-		super();
-		this.id = id;
-		this.instituicao = instituicao;
-		this.finalidade = finalidade;
-		this.titulo = titulo;
-		this.tipo = tipo;
-		DataCriacao = dataCriacao;
-		this.proprietario = proprietario;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Escala other = (Escala) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
 
 }
--- src/main/java/com/databuilder/com/br/escalafacil/domain/Proprietario.java
+++ src/main/java/com/databuilder/com/br/escalafacil/domain/Proprietario.java
@@ -12,6 +12,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -21,11 +22,12 @@ import lombok.Setter;
  * Created by Wendell Clive - email: wendell.clive@gmail.com 14/09/2018
  */
 
-@NoArgsConstructor
 @Entity
+@Table(name = "proprietario")
+@NoArgsConstructor
 @Getter
 @Setter
-@Table(name = "proprietario")
+@EqualsAndHashCode
 public class Proprietario implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -39,7 +41,7 @@ public class Proprietario implements Serializable {
 	private Date dataDeNascimento;
 	private Integer status;
 	private Integer tentativasDeAcesso;
-
+	
 	@OneToMany(mappedBy="proprietario")
 	public List<Escala> escalas = new ArrayList<>();
 
@@ -53,31 +55,6 @@ public class Proprietario implements Serializable {
 		this.dataDeNascimento = dataDeNascimento;
 		this.status = status;
 		this.tentativasDeAcesso = tentativasDeAcesso;
-	}
-
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Proprietario other = (Proprietario) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
 	}
 
 }
