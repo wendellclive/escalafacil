29f3ebdde7a1ff68e0b2cb24aa9413a830baf8e6
Wendell Clive Santos de Lira <wendell.clive@gmail.com>
2018-09-21 16:40:17 -0300

    Deletando um Grupo com DELETE

--- src/main/java/com/databuilder/com/br/escalafacil/resources/GrupoResource.java
+++ src/main/java/com/databuilder/com/br/escalafacil/resources/GrupoResource.java
@@ -58,4 +58,12 @@ public class GrupoResource {
 		
 	}
 	
+	// Associado ao verbo HTTP
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+
+	}
 }
--- src/main/java/com/databuilder/com/br/escalafacil/services/GrupoService.java
+++ src/main/java/com/databuilder/com/br/escalafacil/services/GrupoService.java
@@ -3,10 +3,12 @@ package com.databuilder.com.br.escalafacil.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.databuilder.com.br.escalafacil.domain.Grupo;
 import com.databuilder.com.br.escalafacil.repositories.GrupoRepository;
+import com.databuilder.com.br.escalafacil.services.exceptions.DataIntegrityException;
 import com.databuilder.com.br.escalafacil.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -21,19 +23,28 @@ public class GrupoService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Grupo.class.getName()));
 	}
-	
+
 	public Grupo insert(Grupo obj) {
-		
-		obj.setId(null); //faz o método entender que se não houver ID então é uma alteração
+
+		obj.setId(null); // faz o método entender que se não houver ID então é uma alteração
 		return reposit.save(obj);
-		
+
 	}
-	
+
 	public Grupo update(Grupo obj) {
-		
+
 		find(obj.getId());
 		return reposit.save(obj);
-		
+
 	}
-	
+
+	public void delete(Integer id) {
+
+		find(id);
+		try {
+			reposit.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel Excluir um Grupo");
+		}
+	}
 }
